d2276abbe6cf1ef954e938f2db31a65d82c11609
Matt <mattloppie@gmail.com>
2016-04-23 14:14:32 -0400

    schema update

--- data/schema.sql
+++ data/schema.sql
@@ -16,6 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `boolean`
+--
+USE smartroof;
+DROP TABLE IF EXISTS `boolean`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `boolean` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `value` int(11) NOT NULL,
+  `title` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `boolean`
+--
+
+LOCK TABLES `boolean` WRITE;
+/*!40000 ALTER TABLE `boolean` DISABLE KEYS */;
+INSERT INTO `boolean` VALUES (1,0,'Disabled'),(2,1,'Enabled');
+/*!40000 ALTER TABLE `boolean` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `panels`
 --
 
@@ -26,6 +51,8 @@ CREATE TABLE `panels` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `panel_name` varchar(45) NOT NULL,
   `installed_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `x_points` varchar(45) NOT NULL,
+  `y_points` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   UNIQUE KEY `panel_name_UNIQUE` (`panel_name`)
@@ -52,6 +79,11 @@ CREATE TABLE `sensors` (
   `id` int(6) unsigned NOT NULL AUTO_INCREMENT,
   `sensor_name` varchar(30) NOT NULL,
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `x` int(10) unsigned NOT NULL,
+  `y` int(10) unsigned NOT NULL,
+  `thermal` bit(1) NOT NULL,
+  `visual` bit(1) NOT NULL,
+  `tactile` bit(1) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `sensor_name_UNIQUE` (`sensor_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -88,6 +120,31 @@ LOCK TABLES `settings` WRITE;
 /*!40000 ALTER TABLE `settings` DISABLE KEYS */;
 /*!40000 ALTER TABLE `settings` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `tiles`
+--
+
+DROP TABLE IF EXISTS `tiles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tiles` (
+  `id` int(10) unsigned NOT NULL,
+  `x` int(10) unsigned NOT NULL,
+  `y` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tiles`
+--
+
+LOCK TABLES `tiles` WRITE;
+/*!40000 ALTER TABLE `tiles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tiles` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -98,4 +155,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-22 22:18:11
+-- Dump completed on 2016-04-23 14:10:09
