d6f65a80a7c88aa39aeaa3dbf8b67d089fe16b3f
Matt <mattloppie@gmail.com>
2016-04-22 22:31:37 -0400

    Administration pages

---
+++ application/controllers/config.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: asterixslashstar
+ * Date: 4/22/2016
+ * Time: 10:14 PM
+ */
+class Config extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->database();
+        $this->load->helper('url');
+
+        $this->load->library('grocery_CRUD');
+    }
+
+    public function _config_output($output = null)
+    {
+        $this->load->view('config.php',$output);
+    }
+
+
+    public function sensor_management()
+    {
+        try{
+            $crud = new grocery_CRUD();
+
+            $crud->set_theme('datatables');
+            $crud->set_table('sensors');
+            $crud->set_subject('sensors');
+            $crud->required_fields('sensor_name', 'reg_date');
+            $crud->columns('sensor_name', 'reg_date');
+
+            $output = $crud->render();
+
+            $this->_config_output($output);
+
+        }catch(Exception $e){
+            show_error($e->getMessage().' --- '.$e->getTraceAsString());
+        }
+    }
+
+    public function panel_management()
+    {
+        try{
+            $crud = new grocery_CRUD();
+
+            $crud->set_theme('datatables');
+            $crud->set_table('panels');
+            $crud->set_subject('Panels');
+            $crud->required_fields('panel_name', 'installed_date');
+            $crud->columns('panel_name', 'installed_date');
+
+            $output = $crud->render();
+
+            $this->_config_output($output);
+
+        }catch(Exception $e){
+            show_error($e->getMessage().' --- '.$e->getTraceAsString());
+        }
+    }
+
+    public function settings_management()
+    {
+        try{
+            $crud = new grocery_CRUD();
+
+            $crud->set_theme('datatables');
+            $crud->set_table('settings');
+            $crud->set_subject('Settings');
+            $crud->required_fields('settingscol');
+            $crud->columns('settingscol');
+
+            $output = $crud->render();
+
+            $this->_config_output($output);
+
+        }catch(Exception $e){
+            show_error($e->getMessage().' --- '.$e->getTraceAsString());
+        }
+    }
+}
---
+++ application/views/config.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php 
+foreach($css_files as $file): ?>
+	<link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+<?php endforeach; ?>
+<?php foreach($js_files as $file): ?>
+	<script src="<?php echo $file; ?>"></script>
+<?php endforeach; ?>
+<style type='text/css'>
+body
+{
+	font-family: Arial;
+	font-size: 14px;
+}
+a {
+    color: blue;
+    text-decoration: none;
+    font-size: 14px;
+}
+a:hover
+{
+	text-decoration: underline;
+}
+</style>
+</head>
+<body>
+	<div>
+		<a href='<?php echo site_url('config/sensor_management')?>'>Sensors</a> |
+		<a href='<?php echo site_url('config/panel_management')?>'>Panels</a> |
+		<a href='<?php echo site_url('config/settings_management')?>'>Settings</a>
+		
+	</div>
+	<div style='height:20px;'></div>  
+    <div>
+		<?php echo $output; ?>
+    </div>
+</body>
+</html>
