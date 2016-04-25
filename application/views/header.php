3aa507805ffbbd141144ba25ae73b985889e75f3
Matt <mattloppie@gmail.com>
2016-04-25 00:56:51 -0400

    Header logo

--- application/views/header.php
+++ application/views/header.php
@@ -19,9 +19,10 @@
 
             <div class="masthead clearfix">
                 <div class="inner">
-                    <h3 class="masthead-brand">SmartRoof</h3>
+                    <!--h3 class="masthead-brand">SmartRoof</h3-->
+                    <div style="text-align: left; margin-left: -150px; margin-top: -2px; z-index: -200;"><img src="/SmartRoofNIOTH/assets/logo_5.png" width="150" height="117"></div>
                     <nav>
-                        <ul class="nav masthead-nav">
+                        <ul class="nav masthead-nav" style="margin-right: -150px; margin-top: -30px;">
                             <a href="/SmartRoofNIOTH/">Home</a> |
                             <a href="/SmartRoofNIOTH/index.php/welcome/chart">Chart</a> |
                             <a href="/SmartRoofNIOTH/index.php/config/tile_management">Admin</a>
